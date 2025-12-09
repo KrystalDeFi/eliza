108b8ef1f8627cf77d6dfa11fb7fc7c73c48f80f
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2025-12-09 10:43:32 +0700

    try add idleTimeoutMillis = 0 to mitigate insert error

--- packages/plugin-sql/src/pg/manager.ts
+++ packages/plugin-sql/src/pg/manager.ts
@@ -10,7 +10,7 @@ export class PostgresConnectionManager {
   constructor(connectionString: string, rlsServerId?: string) {
     // If RLS is enabled, set application_name to the server_id
     // This allows the RLS function current_server_id() to read it
-    const poolConfig: PoolConfig = { connectionString };
+    const poolConfig: PoolConfig = { connectionString, idleTimeoutMillis: 0 };
 
     if (rlsServerId) {
       poolConfig.application_name = rlsServerId;
